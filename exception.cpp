93dbf32881bd4ce47de88b1e0227662b5004d960
dac37 <dac37@met.edu>
2024-09-26 12:25:49 +0530

    add exception

---
+++ exception.cpp
@@ -0,0 +1,23 @@
+vi computation.cpp
+c++ -c computation.cpp 
+vi computation.h
+objdump -t computation.o
+vi computation.h
+vi computation.cpp 
+
+
+
+//////////////////////////Exception//////////////////////////////////
+
+
+ vi exceptiontest.cpp
+ c++ exceptiontest.cpp -o program 
+ c++ exceptiontest.cpp computation.o -o program 
+ ./program 
+ vi exceptiontest.cpp
+ c++ exceptiontest.cpp computation.o -o program 
+ ./program 
+ vi exceptiontest.cpp
+ c++ exceptiontest.cpp computation.o -o program 
+ ./program 
+
