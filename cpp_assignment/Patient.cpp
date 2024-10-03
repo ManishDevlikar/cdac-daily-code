7b6d9d99433f20578e5bda6d344bf534c86ace88
manish devlikar <manish.devlikar5003@gmail.com>
2024-10-04 00:18:26 +0530

    add assignment

---
+++ cpp_assignment/Patient.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+enum BedType {
+    VIP = 500,
+    GENERAL = 250,
+    SEMI_PRIVATE = 350,
+    PRIVATE = 450,
+    ICU = 700,
+    DELUXE = 600
+};
+
+class Patient {
+private:
+    static int patientId;
+    int id;     
+    string name;
+    int age;
+    BedType bedType;
+    int noOfDays;
+
+public:
+    // Constructor
+    Patient(string pname, int page, BedType pbedType, int pnoOfDays)
+        : name(pname), age(page), noOfDays(pnoOfDays) {
+            id=++patientId;  // Increment static patientId for each new patient
+        bedType=pbedType;
+    }
+
+    // Method to calculate bill amount
+    double getBillAmount() {
+        double price = 0;
+        switch (bedType) {
+        case VIP:
+            price = 500;
+            break;
+        case GENERAL:
+            price = 250;
+            break;
+        case SEMI_PRIVATE:
+            price = 350;
+            break;
+        case PRIVATE:
+            price = 450;
+            break;
+        case ICU:
+            price = 700;
+            break;
+        case DELUXE:
+            price = 600;
+            break;
+        default:
+            price = 250;
+            break;
+        }
+        return price * noOfDays;
+    }
+
+    // Method to display patient details and bill amount
+    void display() {
+        cout << "Patient ID: " << patientId << endl;
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Number of Days: " << noOfDays << endl;
+        cout << "Total Bill Amount: " << getBillAmount() << endl;
+    }
+};
+
+// Initialize static variable
+int Patient::patientId = 100;
+
+int main() {
+    // Create a Patient object
+    Patient p1("Ana", 28, VIP, 3);
+    Patient p2("Ana", 28, VIP, 3);
+    // Display patient details and bill amount
+    p1.display();
+    p2.display();
+
+    return 0;
+}
