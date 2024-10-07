8be50a7e7f41ae98f23941a31d3859b44c9eb341
manishdevlikar <manish.devlikar5003@gmail.com>
2024-10-07 15:19:16 +0530

    minor change

--- DotNetAssignments/Patient/Program.cs
+++ DotNetAssignments/Patient/Program.cs
@@ -5,19 +5,17 @@ class Program{
     public static double getTax(Patient patient) {
         double bill=patient.getBillAmount(patient);
         double tax=0;
-        if(bill>1000 && bill<=2000)
+        if(bill>5000 && bill<10000)
+            tax=0.05;
+        else if(bill>10000)
             tax=0.1;
-        else if(bill>2000 && bill<=3000)
-            tax=0.15;
-        else if(bill>3000)
-        tax=0.2;
         return tax*bill;
     }
 
     public static void Main(string[] args){
 
-    Patient patient = new Patient("saas",15,BedType.Vip);
-    InHousePatient inHousePatient = new InHousePatient("react",15,BedType.Vip,5);
+    Patient patient = new Patient("saas",30,BedType.Vip);
+    InHousePatient inHousePatient = new InHousePatient("react",25,BedType.Vip,5);
 
 
     Console.WriteLine($"PatientID: {patient.PatientID}, Patient Name: {patient.PatientName}, Bed Type: {patient.bedType}");
@@ -25,11 +23,10 @@ class Program{
 
    double bill1= patient.getBillAmount(patient);
    double bill2= inHousePatient.getBillAmount(inHousePatient);
-   double bill3= patient.getBillAmount(inHousePatient);
 
-   Console.WriteLine($"Patient Bill: {bill1}, Tax Amount: {getTax(patient)}");
-   Console.WriteLine($"InHousePatient Bill: {bill2}, Tax Amount: {getTax(patient)}");
-   Console.WriteLine($"InHousePatient Without Discount: {bill3}, Tax Amount: {getTax(patient)}");
+
+   Console.WriteLine($"Patient Bill: {bill1}, Tax Amount: {getTax(patient)} Total Amount: {bill1 + getTax(patient)}");
+   Console.WriteLine($"InHousePatient Bill: {bill2}, Tax Amount: {getTax(inHousePatient)} Total Amount: {bill2 + getTax(inHousePatient)}");
     } 
 
 }
