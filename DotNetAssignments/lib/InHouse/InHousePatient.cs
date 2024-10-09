c1f549edafca1f22530146ae5e98a02b70b515de
ManishDevlikar <manish.devlikar5003@gmail.com>
2024-10-09 08:43:03 +0530

    Add files via upload
    add project patient

---
+++ DotNetAssignments/lib/InHouse/InHousePatient.cs
@@ -0,0 +1,20 @@
+﻿using hospital;
+
+
+  class InHousePatient : Patient
+    {
+        public double discount;
+            
+             public InHousePatient(string name,int noOfDays, BedType bedType, double discount ) : base(name, noOfDays, bedType)
+        {
+            this.discount = discount/100;
+        }
+
+
+
+        public override double getBillAmount(Patient p)
+        {
+            return base.getBillAmount(p) - (base.getBillAmount(p) * discount);
+        }
+
+    }
