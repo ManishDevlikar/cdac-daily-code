9c355e12b3e9615cb7256bbe75345132c9cd45a5
manishdevlikar <manish.devlikar5003@gmail.com>
2024-10-09 11:19:52 +0530

    9 oct morning

---
+++ DotNetAssignments/PatientAbs/PatientLib/patient.cs
@@ -0,0 +1,9 @@
+﻿namespace Patient;
+
+public abstract class patient
+{
+    public long Id {get; internal set; }
+    public string PatientName { get; protected set; }
+
+    public int Days { get; protected set; }
+}
