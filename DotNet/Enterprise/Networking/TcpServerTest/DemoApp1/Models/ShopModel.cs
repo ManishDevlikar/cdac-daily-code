ee44d8367dab27e3eed8949b34dc1378ca9344a9
manishdevlikar <manish.devlikar5003@gmail.com>
2024-10-19 11:44:08 +0530

    19 oct morning

--- DotNet/Enterprise/Networking/TcpServerTest/DemoApp1/Models/ShopModel.cs
+++ DotNet/Enterprise/Networking/TcpServerTest/DemoApp1/Models/ShopModel.cs
@@ -1,24 +1,27 @@
-using System.Net.Sockets;
+using System.Text.Json;
 
 namespace DemoApp.Models;
 
-public class ShopModel(string server)
+public class ShopModel
 {
-    public async Task<ItemInfo> ReadItemInfoAsync(string name)
+    private ItemInfo[] items;
+
+    public ShopModel()
     {
-        //Step 1
-        var connection = new TcpClient(server, 4000);
-        //Step 2
-        var remote = connection.GetStream();
-        using var reader = new StreamReader(remote);
-        using var writer = new StreamWriter(remote) { AutoFlush = true };
-        await reader.ReadLineAsync(); //read greeting message
-        await writer.WriteLineAsync(name);
-        string message = await reader.ReadLineAsync();
-        //Step 3
-        connection.Close();
-        if(message != null)
-            return ItemInfo.Parse(message);
-        return default;
+        using var input = new FileStream("EviTek.store", FileMode.Open);
+        //converting a series of bytes int an object is called object deserialization
+        items = JsonSerializer.Deserialize<ItemInfo[]>(input);
+    }
+
+    public virtual ItemInfo GetItemInfo(string name)
+    {
+        return items.FirstOrDefault(i => i.Id == name);
+    }
+
+    public virtual void Save()
+    {
+        using var output = new FileStream("EviTek.store", FileMode.Create);
+        //converting object into series of bytes is called object serialization 
+        JsonSerializer.Serialize(output, items);
     }
 }
--- DotNet/Enterprise/Networking/TcpServerTest/DemoApp1/Program.cs
+++ DotNet/Enterprise/Networking/TcpServerTest/DemoApp1/Program.cs
@@ -1,10 +1,5 @@
-﻿using DemoApp.Models;
+﻿using DemoApp;
+using DemoApp.Models;
 
-string item = args[0].ToLower();
-int quantity = int.Parse(args[1]);
-var model = new ShopModel(args[2]);
-ItemInfo info = model.ReadItemInfoAsync(item).Result;
-if(quantity <= info.Stock)
-    Console.WriteLine("Total Payment: {0:0.00}", 1.08 * quantity * info.Cost);
-else
-    Console.WriteLine("Not available!");
+var app = new ShopWorker(4000, new ShopModel());
+app.Run();
