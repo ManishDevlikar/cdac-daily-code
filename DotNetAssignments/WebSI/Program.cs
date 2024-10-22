d1cd997b10e1206859bbf3b0fce4bf0c703fbb25
manishdevlikar <manish.devlikar5003@gmail.com>
2024-10-22 15:18:34 +0530

    web si ci

---
+++ DotNetAssignments/WebSI/Program.cs
@@ -0,0 +1,10 @@
+﻿using DemoApp.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.MapGet("/Si", Greeting.Welcome);
+app.MapPost("/Simple", Greeting.Hello);    
+app.MapGet("/Ci", Greeting.CompoundForm);
+app.MapPost("/Compound", Greeting.Compound);
+
+app.Run();
