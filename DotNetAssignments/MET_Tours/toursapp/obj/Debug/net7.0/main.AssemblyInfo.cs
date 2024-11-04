96a28b5a8997489bfe98ae3c95b6f88071de69c8
manish devlikar <manish.devlikar5003@gmail.com>
2024-11-04 23:24:41 +0530

    4 nov

--- DotNetAssignments/MET_Tours/toursapp/obj/Debug/net7.0/main.AssemblyInfo.cs
+++ DotNetAssignments/MET_Tours/toursapp/obj/Debug/net7.0/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1d81673bf8cb864bdfa9b1e26c6c56b0a9bc3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DotNetAssignments/Patient/obj/Debug/net7.0/Patient.AssemblyInfo.cs
+++ DotNetAssignments/Patient/obj/Debug/net7.0/Patient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1d81673bf8cb864bdfa9b1e26c6c56b0a9bc3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
