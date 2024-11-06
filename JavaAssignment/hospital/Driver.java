b44e5cdb676fb83bc1f3d85a6f500579fe8b3c76
manishdevlikar <manish.devlikar5003@gmail.com>
2024-11-06 12:05:16 +0530

    06 nov morning

--- JavaAssignment/hospital/Driver.java
+++ JavaAssignment/hospital/Driver.java
@@ -1,9 +1,10 @@
 import hospital.BedType;
 import hospital.InHousePatient;
+import hospital.Patient;
 
 public class Driver {
     public static void main(String[] args) {
-        InHousePatient patient = new InHousePatient(1,"manish",3,2.4,BedType.General);
+        Patient patient = new InHousePatient(1,"manish",3,2.4,BedType.General);
         var val=patient.getBill();
         System.out.println(val);
     }
